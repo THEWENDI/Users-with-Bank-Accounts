f4a83205b351ef604d7d19c453b889f798a7300a
Wendi Li <issacbbb@hotmaill.com>
2022-01-12 13:59:16 -0800

    initial commit

--- users-bankaccount.py
+++ users-bankaccount.py
@@ -19,8 +19,8 @@ class BankAccount:
         return self
     
     def display_account_info(self):
-        print("Balance: ",self.balance)
-        return self
+        return self.balance
+        
 
     def yield_interest(self):
         if(self.balance > 0):
@@ -46,19 +46,77 @@ BankAccount.print_all_accounts()
 class User:
     def __init__(self, name):
         self.name = name
-        self.account = BankAccount()
-    
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
-        return self
-    
-    def make_withdrawal(self, amount):
-        self.account.withdraw(amount)
-        return self
+        self.account = {
+            "checking" : BankAccount(),
+            "saving" : BankAccount()
+        }
     
     def display_user_balance(self):
-        self.account.display_account_info()
+        print("User: ",self.name , "Checking Balance: ",self.account['checking'].display_account_info())
 
 tom = User("tom")
 
-tom.make_deposit(1000).display_user_balance()
+tom.account['checking'].deposit(1000)
+tom.display_user_balance()
+
+# # answer:
+# class BankAccount:
+#     accounts = []
+#     def __init__(self,int_rate,balance):
+#         self.int_rate = int_rate
+#         self.balance = balance
+#         BankAccount.accounts.append(self)
+
+#     def deposit(self, amount):
+#         self.balance += amount
+#         return self
+
+#     def withdraw(self,amount):
+#         if(self.balance - amount) >= 0:
+#             self.balance -= amount
+#         else:
+#             print("Insufficient Funds: Charging a $5 fee")
+#             self.balance -= 5
+#         return self
+    
+#     def display_account_info(self):
+#         return f"{self.balance}"
+
+#     def yeild_interest(self):
+#         if self.balance > 0:
+#             self.balance += (self.balance * self.int_rate)
+#         return self
+
+#     @classmethod
+#     def print_all_accounts(cls):
+#         for account in cls.accounts:
+#             account.display_account_info()
+
+
+# class User:
+
+#     def __init__(self, name):
+#         self.name = name
+#         self.account = {
+#             "checking" : BankAccount(.02,1000),
+#             "savings" : BankAccount(.05,3000)
+#         }
+        
+
+#     def display_user_balance(self):
+#         print(f"User: {self.name}, Checking Balance: {self.account['checking'].display_account_info()}")
+#         print(f"User: {self.name}, Savings Balance: {self.account['savings'].display_account_info()}")
+#         return self
+
+#     def transfer_money(self,amount,user):
+#         self.amount -= amount
+#         user.amount += amount
+#         self.display_user_balance()
+#         user.display_user_balance()
+#         return self
+
+
+# adrien = User("Adrien")
+
+# adrien.account['checking'].deposit(100)
+# adrien.display_user_balance()
